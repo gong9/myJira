3606c7afe845fc3e19b36878ad263c1b3738c129
gongzhen <gongzhen@huohua.cn>
2021-06-21 10:06:35 +0800

    fix :fix some style

--- src/view/Login/index.tsx
+++ src/view/Login/index.tsx
@@ -1,13 +1,19 @@
+/* eslint-disable import/no-anonymous-default-export */
 
 import { Form, Input, Button } from 'antd'
-//注册及登录页面
+import './index.css'
+
+/**
+ * login page
+ */
+
 export default () => {
     const onFinish = (values: { username: string, password: string }) => {
         console.log(values)
     }
     return (
-        <div>
-            <Form onFinish={onFinish}>
+        <div className='login'>
+            <Form onFinish={onFinish} className='login-form'>
                 <Form.Item
                     label="用户名"
                     name="username"
@@ -22,7 +28,7 @@ export default () => {
                     <Input.Password />
                 </Form.Item>
                 <Form.Item>
-                    <Button type="primary" htmlType="submit">
+                    <Button type="primary" htmlType="submit" className='login-submit'>
                         登录
                     </Button>
                 </Form.Item>
